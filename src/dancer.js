0d465781c2327dea3f2b1b8802b330f39dd5290b
Dakota-Peel <koteman624@gmail.com>
2015-12-01 10:20:47 -0800

    adding audio file

--- src/dancer.js
+++ src/dancer.js
@@ -5,6 +5,7 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.timeBetweenSteps = timeBetweenSteps;
+  this.moving = true;
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
@@ -15,14 +16,12 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
 
   makeDancer.prototype.step = function() {
-    
-    
+    if(this.moving){
+
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
     setTimeout(this.step.bind(this), this.timeBetweenSteps);
-                      
-
-
+  }
   };
 
   makeDancer.prototype.setPosition = function(top, left) {
@@ -37,6 +36,8 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   };
 
   makeDancer.prototype.lineUp = function(){
-  
-    this.$node.css({left: 100});
+    this.moving = false;
+    this.$node.stop();
+    this.$node.css({left: 500});
+    this.moving = true;
   };
